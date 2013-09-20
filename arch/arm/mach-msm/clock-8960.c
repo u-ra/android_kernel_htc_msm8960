43f88658dddde548730bbff82c35c0570c1ab5a5
h8rift <h8rift@gmail.com>
2013-09-20 07:17:35 -0500

    bcmdhd: m4: add clocks and defconfig settings for bluetooth
    Change-Id: I9c3da4aad9065a8ec9134f2a2bd474a91e03c322

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -6063,6 +6063,9 @@ static struct clk_lookup msm_clocks_8930[] = {
 	CLK_LOOKUP("core_clk",		gsbi4_uart_clk.c,	""),
 	CLK_LOOKUP("core_clk",		gsbi5_uart_clk.c, "msm_serial_hsl.0"),
 	CLK_LOOKUP("core_clk",		gsbi6_uart_clk.c, "msm_serial_hs.0"),
+#ifdef CONFIG_SERIAL_MSM_HS_BRCM
+	CLK_LOOKUP("core_clk",		gsbi6_uart_clk.c, "msm_serial_hs_brcm.0"),
+#endif	
 	CLK_LOOKUP("core_clk",		gsbi7_uart_clk.c,	""),
 	CLK_LOOKUP("core_clk",		gsbi8_uart_clk.c,	""),
 	/* used on 8930 SGLTE for Primary IPC */
@@ -6119,6 +6122,9 @@ static struct clk_lookup msm_clocks_8930[] = {
 	CLK_LOOKUP("iface_clk",		gsbi4_p_clk.c,		"qup_i2c.4"),
 	CLK_LOOKUP("iface_clk",		gsbi5_p_clk.c,	"msm_serial_hsl.0"),
 	CLK_LOOKUP("iface_clk",		gsbi6_p_clk.c,  "msm_serial_hs.0"),
+#ifdef CONFIG_SERIAL_MSM_HS_BRCM
+	CLK_LOOKUP("iface_clk",		gsbi6_p_clk.c,	"msm_serial_hs_brcm.0"),
+#endif
 	CLK_LOOKUP("iface_clk",		gsbi7_p_clk.c,		""),
 	/* used on 8930 SGLTE for Camera */
 	CLK_LOOKUP("iface_clk",		gsbi8_p_clk.c,		"qup_i2c.8"),
