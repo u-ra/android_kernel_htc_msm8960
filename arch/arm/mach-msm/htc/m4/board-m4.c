24e539a9b7cfd63b8de36336727d3e0b5c1241a6
Jurica Vukadin <u.ra604@gmail.com>
2014-02-27 18:09:38 +0100

    m4: add ext_5v platform device, needed for OTG
    Gets rid of a ton of these:
    <3>[    0.090767] msm-gpio-regulator msm-gpio-regulator.97: Failed to find supply ext_5v
    <3>[    0.090889] gpio_vreg_probe: ext_otg_sw: regulator_register failed, rc=-517.
    <6>[    0.090920] platform msm-gpio-regulator.97: Driver msm-gpio-regulator requests probe deferral

--- arch/arm/mach-msm/htc/m4/board-m4.c
+++ arch/arm/mach-msm/htc/m4/board-m4.c
@@ -3029,6 +3029,16 @@ static struct platform_device msm8930_device_ext_l2_vreg __devinitdata = {
 #else
 
 /* 8930 Phase 2 */
+static struct platform_device msm8930_device_ext_5v_vreg __devinitdata = {
+	.name	= GPIO_REGULATOR_DEV_NAME,
+	.id	= 63,
+	.dev	= {
+		.platform_data = &m4_gpio_regulator_pdata[
+					MSM8930_GPIO_VREG_ID_EXT_5V],
+	},
+};
+
+/* 8930 Phase 2 */
 static struct platform_device msm8930_device_ext_otg_sw_vreg __devinitdata = {
 	.name	= GPIO_REGULATOR_DEV_NAME,
 	.id	= 97,
@@ -3122,6 +3132,7 @@ static struct platform_device *common_devices[] __initdata = {
 #endif
 	&msm8960_device_ssbi_pmic,
 #ifdef MSM8930_PHASE_2
+	&msm8930_device_ext_5v_vreg,
 	&msm8930_device_ext_otg_sw_vreg,
 #endif
 	&msm8960_device_otg,
